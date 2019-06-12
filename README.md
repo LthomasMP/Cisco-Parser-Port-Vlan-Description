9074a5bcc9e3755d9d6e210c78ccb8309b9833aa
Thomas Leroy <thomas.leroy.mp@gmail.com>
2019-06-12 16:07:54 +0200

    Added possibility to add several file in argument of command line

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# Cisco-Parser-Port-Vlan-Description
+# Cisco Parser Port-Vlan-Description
 Tiny script that finds VLAN and description for each port with a Cisco equipment's show run
 
+## Requirements ##
 This script uses the CiscoConfParse lib, be carrefull of installing it before running the script.
+
+## Use ##
+Just run the script with your files in argument of the command. The script will create a csv file with name, switchport mode, vlan and description. 
+
+``` console
+$ ./parser.py <your_file_1.log> ... <your_file_n.log>
+```
+
+I tried the program with .log files, but it will work for every text file, I think (I hope).
+
+## Lacks ##
+When a port is in trunk mode, my client didn't want to know allowed vlan, that's why vlan ID is set to 0 when it is a trunk port. I will improve it later if I need it.
--- parseur.py
+++ parseur.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-###################### WARNING #################################
+############################ WARNING ###########################
 #   You need at leat a show run command in your file. Without  #
-#   the show run, the parsing won't work                        # 
+#   the show run, the parsing won't work                       # 
 ################################################################
 
 from ciscoconfparse import CiscoConfParse
@@ -22,14 +22,14 @@ class Port:
         self.vlan = vlan
         self.description = description
 
-    def find_mode(self, parse, conf_file):
+    def set_mode(self, parse, conf_file):
         if parse.find_children_w_parents(r'%s' %self.name, r'switchport mode') != []:
             intf = parse.find_children_w_parents(r'%s' %self.name, r'switchport mode')[0]
             self.mode = intf.replace('switchport mode ','').replace(' ','')
         else:
             self.mode = "mode not found"
 
-    def find_vlans(self, parse, conf_file):
+    def set_vlan(self, parse, conf_file):
         if self.mode == 'access':
             if parse.find_children_w_parents(r'%s' %self.name, r'authentication event server dead action authorize vlan') != []:
                 intf = parse.find_children_w_parents(r'%s' %self.name, r'authentication event server dead action authorize vlan')[0]
@@ -41,58 +41,72 @@ class Port:
         else:
             self.vlan = 0
 
-    def find_port_description(self, parse, conf_file):
+    def set_port_description(self, parse, conf_file):
         if parse.find_children_w_parents(r'%s' %self.name, r'description') != []:
             intf = parse.find_children_w_parents(r'%s' %self.name, r'description')[0]
             self.description = intf.replace('description ','')
         else:
             self.description = "Description not found"
 
-def find_every_gigabit_ports(parse, Port_list, conf_file):
+def find_every_gigabit_ports(parse, conf_file):
+    output = []
     intf = parse.find_objects(r'^interface GigabitEthernet')
     for i in range (len(intf)):
-        Port_list.append(intf[i].text.replace('interface ', ''))
-
-def find_every_ten_gigabit_ports(parse, Port_list, conf_file):
+        output.append(intf[i].text.replace('interface ', ''))
+    return output
+    
+def find_every_ten_gigabit_ports(parse, conf_file):
+    output = []
     intf = parse.find_objects(r'^interface TenGigabitEthernet')
     for i in range (len(intf)):
-        Port_list.append(intf[i].text)
+        output.append(intf[i].text)
+    return output
 
-def find_every_fast_ports(parse, Port_list, conf_file):
+def find_every_fast_ports(parse, conf_file):
+    output = []
     intf = parse.find_objects(r'^interface FastEthernet')
     for i in range (len(intf)):
-        Port_list.append(intf[i].text)
+        output.append(intf[i].text)
+    return output
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         sys.exit("Usage: ./parseur.py <your_file>")
 
     else :
-        conf_file = sys.argv[1]
-        parse = CiscoConfParse(conf_file, factory=True)
-        Port_list = []
-        find_every_gigabit_ports(parse, Port_list, conf_file)
-        find_every_ten_gigabit_ports(parse, Port_list, conf_file)
-        find_every_fast_ports(parse, Port_list, conf_file)
-        
-        for i in range(len(Port_list)):
-            port = Port(Port_list[i], "", 0, "Default")
-            port.find_port_description(parse, conf_file)
-            port.find_mode(parse, conf_file)
-            port.find_vlans(parse, conf_file)
-            Port_list[i] = port
+        for i in range (1,len(sys.argv)):
+            conf_file = sys.argv[i]
+            parse = CiscoConfParse(conf_file, factory=True)
+            Port_list = []
+            Gi_port_list = find_every_gigabit_ports(parse, conf_file)
+            Ten_gi_port_list = find_every_ten_gigabit_ports(parse, conf_file)
+            Fa_port_list = find_every_fast_ports(parse, conf_file)
+ 
+            if Gi_port_list != []:
+                Port_list += Gi_port_list
+            if Ten_gi_port_list != []:
+                Port_list += Ten_gi_port_list
+            if Fa_port_list != []:
+                Port_list += Fa_port_list 
 
-        # Clean the path to isolate just <name>.<extension>
-        while conf_file.find('/') != -1:
-            conf_file = conf_file[conf_file.find('/')+1:]
-        
-        print(len(Port_list), " port has been found")
-        with open(conf_file[:conf_file.find('.')] + '-parsed.csv', 'w') as file:
-            writer = csv.writer(file, delimiter=',')
-            writer.writerow(['port','switchport', 'vlan', 'description'])
             for i in range(len(Port_list)):
-                writer.writerow([Port_list[i].name, Port_list[i].mode, Port_list[i].vlan, Port_list[i].description])
-                print(Port_list[i].name, " mode: ", Port_list[i].mode, " vlan: ", str(Port_list[i].vlan), " and description: ", Port_list[i].description) 
+                port = Port(Port_list[i], "", 0, "Default")
+                port.set_port_description(parse, conf_file)
+                port.set_mode(parse, conf_file)
+                port.set_vlan(parse, conf_file)
+                Port_list[i] = port
+
+            # Clean the path to isolate just <name>.<extension>
+            while conf_file.find('/') != -1:
+                conf_file = conf_file[conf_file.find('/')+1:]
+
+            print(len(Port_list), " port has been found")
+            with open(conf_file[:conf_file.find('.')] + '-parsed.csv', 'w') as file:
+                writer = csv.writer(file, delimiter=',')
+                writer.writerow(['port','switchport', 'vlan', 'description'])
+                for i in range(len(Port_list)):
+                    writer.writerow([Port_list[i].name, Port_list[i].mode, Port_list[i].vlan, Port_list[i].description])
+                    print(Port_list[i].name, " mode: ", Port_list[i].mode, " vlan: ", str(Port_list[i].vlan), " and description: ", Port_list[i].description) 
          
 if __name__ == '__main__':
     main()
